ec75d38811affb9c6386fe2bbafef61d085e4f51
Sepehr Vahedi <sepehr.vahedi@gmail.com>
2025-11-15 00:49:14 +0330

    Added logos to the portgolio

--- js/main.js
+++ js/main.js
@@ -86,3 +86,13 @@ if (profileImage) {
         }, 100);
     });
 }
+
+// Add shadow to navbar on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
